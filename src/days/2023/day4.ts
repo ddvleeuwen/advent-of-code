a16a6d966050efd8f4967dc30df7945d95b907c6
ddvleeuwen <ddvleeuwen@wehkamp.nl>
2023-12-06 10:07:06 +0100

    Add second solution of Day 4

--- src/days/2023/day4.ts
+++ src/days/2023/day4.ts
@@ -9,9 +9,25 @@ export default class Day4 extends Solution {
   example = false;
   /* ------------------ */
 
+  getCardValue(cardsToCheck: number[], lines: string[]) {
+    if (cardsToCheck.length === 0) {
+      return 1;
+    }
+
+    let newRes: number = 0;
+
+    for (const crdId of cardsToCheck) {
+      const won = this.getCardsWon(lines[crdId - 1]);
+      newRes += this.getCardValue(won, lines);
+    }
+
+    // test
+    return newRes + 1;
+  }
+
   getCardsWon(line: string) {
     const cardParts = line.split(': ');
-    const cardId = parseInt(cardParts[0].split(' ')[1], 10);
+    const cardId = parseInt(cardParts[0].split(' ').filter((s) => s !== '')[1], 10);
 
     const gameParts = cardParts[1].split(' | ');
     const winningNumbers = gameParts[0].split(' ').filter((s) => s !== '').map((n) => parseInt(n.trim(), 10));
@@ -25,8 +41,11 @@ export default class Day4 extends Solution {
   }
 
   solve(input: string) {
-    for (const line of input.split('\n')) {
+    const lines = input.split('\n');
+    for (const line of lines) {
       const won = this.getCardsWon(line);
+
+      this.b += this.getCardValue(won, lines);
       this.a += Math.floor(2 ** won.length / 2);
     }
   }
